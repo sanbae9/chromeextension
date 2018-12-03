1375b59e0868443edb343f34404e591e3790a041
sanbae9 <sanbae@live.unc.edu>
2018-12-03 15:11:01 -0500

    Completed, fixed bugs

--- content.js
+++ content.js
@@ -1,49 +1,39 @@
 
 var elements = document.getElementsByTagName('*');
 
-// chrome.storage.sync.get(['wordlist'], (e) => {
-//     words = e.wordlist;
-// });
-// chrome.storage.sync.get(['translist'], (e) => {
-//     translations = e.translist;
-// });
+function script() {
+    chrome.storage.sync.get(['wordlist'], function (e) {
+        chrome.storage.sync.get(['translist'], function (v) {
+            words = e.wordlist;
+            console.log(words);
+            translations = v.translist;
+            console.log(translations);
 
-function getWords() {
-    var words = [];
-    chrome.storage.sync.get(['wordlist'], function(e) {
-        words = e.wordlist;
-    });
-    return words;
-}
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+                for (var j = 0; j < element.childNodes.length; j++) {
+                    // We are now looping through every element and child node on the document. 
+                    var node = element.childNodes[j];
+                    if (node.nodeType === 3) {
+                        var text = node.nodeValue;
+                        for (var k = 0; k < words.length; k++) {
+                            if (text.toLowerCase() === words[k].toLowerCase()) {
+                                var newText = text.replace(text, capsIfNeeded(text, translations[k]));
+                                element.replaceChild(document.createTextNode(newText), node);
+                            }
+                        }
 
-function getTranslations() {
-    var translations = [];
-    chrome.storage.sync.get(['translist'], function(e) {
-        words = e.wordlist;
-    });
-    return translations;
-}
-
-// words = ["banana", "apple"];
-// translations = ["plátano", "manzana"];
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    for (var j = 0; j < element.childNodes.length; j++) {
-        // We are now looping through every element and child node on the document. 
-        var node = element.childNodes[j];
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            for (var k = 0; k < getWords.length; k++) {
-                if (text.toLowerCase() === getWords[k].toLowerCase()) {
-                    var newText = text.replace(text, capsIfNeeded(text, getTranslations()[k]));
-                    element.replaceChild(document.createTextNode(newText), node);
+                    }
                 }
             }
-
-        }
-    }
+        });
+    });
 }
+
+script();
+
+
+// Helper Functions // ~~~~~~~~~~~~
 
 function capsIfNeeded(p1, p2) {
     if (p1.charAt(0) === p1.charAt(0).toUpperCase()) {
